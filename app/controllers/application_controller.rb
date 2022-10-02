0ddc707aef8f1ac05086ea18733fce4ed14f3a37
leela khadka <khadkaleela3@gmail.com>
2022-10-02 15:05:37 -0700

    commit tree 3379666578b5b8d2deb2b8d4aa4756940565f4e9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
   get '/' do
     { message: "Hello world" }.to_json
+  end
+
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+  end
+
+  get '/games/:id' do
+    gameId = params[:id].to_i
+    game = Game.find(gameId)
+    game.to_json(include: { reviews: { include: :user } })
+
+
   end
 
 end
